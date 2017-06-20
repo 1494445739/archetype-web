ee74ad51ed745994fdb9d12008df609881840be2
zenglin <zengl@tzg.cn>
2017-06-20 16:35:22 +0800

    fix bug

--- target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/webapp/src/app/app.component.ts
+++ target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/webapp/src/app/app.component.ts
@@ -1,19 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
 declare var BASEPATH: string;
+declare var MENUDATA: Array<any>;
 
 @Component({
   selector: 'app-entry',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  username = '';
+  username = 'aaaa';
   menudata: any[];
 
   constructor() { }
   ngOnInit() {
-    this.menudata = [
-    ];
+    this.menudata = this.convertMenudata();
+  }
+
+  convertMenudata () {
+    const menuMap = new Map<number, any>();
+    const roots = [];
+    const menus = MENUDATA.filter(m => m.category !== 'a')
+    .sort((a, b) => { return a.seq - b.seq; })
+    .map(m => {
+      const menuitem = {
+        id: m.id,
+        pid: m.pid,
+        name: m.name,
+        route: m.category === 'm' ? m.uri : null,
+        children: null
+      };
+      menuMap.set(menuitem.id, menuitem);
+      return menuitem;
+    }).forEach(m => {
+      if (m.pid === 0) {
+        roots.push(m);
+      } else {
+        const parent = menuMap.get(m.pid);
+        if (!parent.children) { parent.children = []; }
+        parent.children.push(m);
+      }
+    });
+
+    return roots;
   }
 
   logout () {
